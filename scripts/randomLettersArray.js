7543b36d707f9f46e9250c911a51acac840fdab3
Leta Keane <letakeane+turing@gmail.com>
2017-03-29 20:48:38 -0600

    make small syntax adjustments

--- scripts/randomLettersArray.js
+++ scripts/randomLettersArray.js
@@ -1,5 +1,5 @@
 const randomLettersArray = array => {
-  let letters = "bcdefghijklmnopqrstuvwxy";
+  var letters = "bcdefghijklmnopqrstuvwxy";
 
   for (let i = 0; i < 5000; i++) {
     array[i] = letters.charAt(Math.floor(Math.random() * letters.length));
